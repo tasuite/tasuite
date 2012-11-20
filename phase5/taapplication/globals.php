6cfa2c310237e1f19559a910c522a46c53b078a3
Adam <atc4cy@virginia.edu>
2012-11-19 19:21:22 -0500

    Committing changes

--- phase5/taapplication/globals.php
+++ phase5/taapplication/globals.php
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 <?php 
 $CURRENT_APPLICATION_YEAR = 2013;
 $CURRENT_APPLICATION_SEMESTER = 'Spring';
+=======
+<?php 
+$CURRENT_APPLICATION_YEAR = 2013;
+$CURRENT_APPLICATION_SEMESTER = 'Spring';
+>>>>>>> Changed it to be functional
  ?>
