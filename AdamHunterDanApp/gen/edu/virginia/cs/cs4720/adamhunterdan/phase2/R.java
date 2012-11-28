b0434d7b83f2ef5ce27bb3ad3226f0dc7facbdc1
Adam <atc4cy@virginia.edu>
2012-11-28 12:44:22 -0500

    Modified the app so all the activities do not display as apps

--- AdamHunterDanApp/gen/edu/virginia/cs/cs4720/adamhunterdan/phase2/R.java
+++ AdamHunterDanApp/gen/edu/virginia/cs/cs4720/adamhunterdan/phase2/R.java
@@ -30,19 +30,20 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_action_search=0x7f020005;
         public static final int ic_launcher=0x7f020006;
         public static final int logo=0x7f020007;
-        public static final int officehours=0x7f020008;
-        public static final int roster=0x7f020009;
-        public static final int white_wallpaper=0x7f02000a;
+        public static final int logofaded=0x7f020008;
+        public static final int officehours=0x7f020009;
+        public static final int roster=0x7f02000a;
+        public static final int white_wallpaper=0x7f02000b;
     }
     public static final class id {
         public static final int AnnouncementsList=0x7f080000;
         public static final int Button01=0x7f080006;
         public static final int blah=0x7f080004;
         public static final int button1=0x7f080007;
-        public static final int gallery=0x7f080001;
-        public static final int imageView1=0x7f080003;
+        public static final int gallery=0x7f080002;
+        public static final int imageView1=0x7f080001;
         public static final int menu_settings=0x7f08000b;
-        public static final int picture=0x7f080002;
+        public static final int picture=0x7f080003;
         public static final int regrade=0x7f080005;
         public static final int regradeList=0x7f08000a;
         public static final int search_message=0x7f080008;
